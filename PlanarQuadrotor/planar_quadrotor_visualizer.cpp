a12aba6552886cf11f996b1a0adb66dfaa6d223e
Lucjuszo <168374655+Lucjuszo@users.noreply.github.com>
2024-06-21 22:02:25 +0200

    Update planar_quadrotor_visualizer.cpp

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -4,12 +4,6 @@
 
 PlanarQuadrotorVisualizer::PlanarQuadrotorVisualizer(PlanarQuadrotor* quadrotor_ptr) : quadrotor_ptr(quadrotor_ptr) {}
 
-/**
- * TODO: Improve visualization
- * 1. Transform coordinates from quadrotor frame to image frame so the circle is in the middle of the screen
- * 2. Use more shapes to represent quadrotor (e.x. try replicate http://underactuated.mit.edu/acrobot.html#section3 or do something prettier)
- * 3. Animate propellers
- */
 void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer>& gRenderer) {
     Eigen::VectorXf state = quadrotor_ptr->GetState();
     float q_x, q_y, q_theta;
